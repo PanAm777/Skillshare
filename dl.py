f84679c2aa822eb3158a4a343fd146bdc79f884b
PanAm777 <137831002+PanAm777@users.noreply.github.com>
2023-06-26 13:12:54 -0700

    Update dl.py

--- dl.py
+++ dl.py
@@ -1,12 +1,10 @@
 import sys, os
 from skillshare import Skillshare, splash
-from magic import cookie
-
 # or by class ID:
 # dl.download_course_by_class_id(189505397)
 
 def main():
-    dl = Skillshare(cookie)
+    dl = Skillshare("PHPSESSID=Your PHPSESSID value")
     course_url = sys.argv[1]
     dl.download_course_by_url(course_url)
 
@@ -14,3 +12,7 @@ def main():
 if __name__ == "__main__":
     splash()
     main()
+
+
+
+
